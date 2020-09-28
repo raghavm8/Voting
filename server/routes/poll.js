f43bd2fe3735af0a2e781153491fa3d6ee1b5eee
raghavm8 <raghav.mittal1109@gmail.com>
2020-09-28 18:40:54 +0530

    code updated

--- server/routes/poll.js
+++ server/routes/poll.js
@@ -4,6 +4,11 @@ const auth = require('../middleware/auth');
 
 router.route('/').get(handle.showPoll); // get everything
 router.route('/').post(auth,handle.createPoll); // add a new poll
-router.get()
+
+router.get('/user',auth,handle.usersPolls);
+
+router.route('/:id').get(handle.getPoll);
+router.route('/:id').post(auth,handle.vote)
+router.route('/:id').delete(auth, handle.deletePoll);
 
 module.exports = router
